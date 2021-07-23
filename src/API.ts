fe3b0009b51ae0824765d49007443c8e5f8548aa
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-07-23 20:08:39 +0600

    typescript quiz app

--- src/API.ts
+++ src/API.ts
@@ -1,5 +1,4 @@
-// import { type } from "os";
-import { shuffelArray } from "./utils";
+import { shuffleArray } from './utils';
 
 export type Question = {
   category: string;
@@ -16,23 +15,13 @@ export enum Difficulty {
   HARD = "hard",
 }
 
-export type QuestionState = Question & { answers: string[] };
+export type QuestionsState = Question & { answers: string[] };
 
-export const fetchQuizQuestions = async (
-  amount: number,
-  difficulty: Difficulty
-) => {
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  // console.log(data);
-  
-  return data.results.map((question: Question) => (
-    {
-      ...question,
-      answaers: shuffelArray([
-        ...question.incorrect_answers,
-        question.correct_answer,
-      ]),
-    }
-  ));
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
 };
